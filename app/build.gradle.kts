be6c1eda9f723f71be23ee450f9dfed119a93dbb
Rishuke <balasubramaniam.rishi@gmail.com>
2024-07-24 20:34:56 +0200

    Merge All build to one

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.esgi.promocare_android"
         minSdk = 27
         targetSdk = 34
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
