a685b7641ae9be81efc62c08b874e37c18257073
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-07-25 20:18:10 +0200

    Fix of conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.esgi.promocare_android"
         minSdk = 27
         targetSdk = 34
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
